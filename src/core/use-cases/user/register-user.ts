73386139377f125ee057d70e5d7c1b3437df78f4
Felipe José <lipej@pm.me>
2022-12-06 14:25:14 -0300

    refactor: use base use case contract

--- src/core/use-cases/user/register-user.ts
+++ src/core/use-cases/user/register-user.ts
@@ -1,11 +1,14 @@
 import {IUserRepository} from '#core/contracts/repositories/user';
+import {IBaseUseCase} from '#core/contracts/use-cases/base';
 import {User} from '#core/entities/user';
 import {RegisterUserDTO} from '../dtos/user/register-user';
 
-export class RegisterUserUseCase {
+export class RegisterUserUseCase
+  implements IBaseUseCase<RegisterUserDTO, void>
+{
   constructor(private readonly userRepo: IUserRepository) {}
 
-  execute = async (args: RegisterUserDTO) => {
+  perform = async (args: RegisterUserDTO) => {
     const user = User.create({username: args.username, email: args.email});
 
     await this.userRepo.create(user);
