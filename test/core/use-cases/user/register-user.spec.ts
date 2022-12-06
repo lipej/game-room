c0b24b6893a6fdd8440195f57e12564ce94955a9
Felipe José <lipej@pm.me>
2022-12-06 14:26:31 -0300

    test: add fail scenery to use case

--- test/core/use-cases/user/register-user.spec.ts
+++ test/core/use-cases/user/register-user.spec.ts
@@ -1,5 +1,6 @@
 import {IUserRepository} from '#core/contracts/repositories/user';
 import {User} from '#core/entities/user';
+import {ValidationError} from '#core/errors/validation';
 import {RegisterUserUseCase} from '#core/use-cases/user/register-user';
 import {UserRepositoryInMemory} from '#infra/repositories/in-memory/user';
 import {userParams} from '?test/mocks/user';
@@ -16,8 +17,16 @@ describe(RegisterUserUseCase.name, () => {
   });
 
   it('should register a user', async () => {
-    const promise = useCase.execute(userParams);
+    const promise = useCase.perform(userParams);
 
-    await expect(promise).resolves;
+    await expect(promise).resolves.toBeUndefined();
+  });
+
+  it('should not register a user with invalid email', async () => {
+    const promise = useCase.perform({...userParams, email: '123#123.com'});
+
+    await expect(promise).rejects.toThrowError(
+      new ValidationError('email', '123#123.com')
+    );
   });
 });
