ca205de54fe01db84b522e64cba695a164ea35fd
Felipe José <lipej@pm.me>
2022-12-07 14:37:07 -0300

    test: jwt token manager implementation

---
+++ test/data/protocols/jwt/token-manager.spec.ts
@@ -0,0 +1,25 @@
+import {ITokenManager} from '#core/contracts/protocols/token-manager';
+import {User} from '#core/entities/user';
+import {TokenManagerJWT} from '#data/protocols/jwt/token-manager';
+import {userParams} from '?test/mocks/user';
+
+describe(TokenManagerJWT.name, () => {
+  const user = User.create(userParams);
+  const manager: ITokenManager = new TokenManagerJWT();
+
+  it('should sign a token', () => {
+    expect(manager.sign(user)).not.toBeUndefined();
+  });
+
+  it('should return true in verify if token is valid', () => {
+    const token = manager.sign(user);
+
+    expect(manager.verify(token)).toBeTruthy();
+  });
+
+  it('should return false in verify if token is invalid', () => {
+    const token = 'invalid_token';
+
+    expect(manager.verify(token)).toBeFalsy();
+  });
+});
