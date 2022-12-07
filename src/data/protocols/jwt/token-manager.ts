4c3cd7b1f5b1396cd716115380aac0d0de86e474
Felipe José <lipej@pm.me>
2022-12-07 14:37:01 -0300

    feat: jwt token manager implementation

---
+++ src/data/protocols/jwt/token-manager.ts
@@ -0,0 +1,21 @@
+import {ITokenManager} from '#core/contracts/protocols/token-manager';
+import {User} from '#core/entities/user';
+import * as jwt from 'jsonwebtoken';
+
+export class TokenManagerJWT implements ITokenManager {
+  private readonly PRIVATE_KEY: jwt.Secret = 'aaaa';
+
+  sign = (user: User): string =>
+    jwt.sign(this.genPayload(user.email), this.PRIVATE_KEY);
+
+  verify = (token: string): boolean => {
+    try {
+      jwt.verify(token, this.PRIVATE_KEY);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  };
+
+  private genPayload = (email: string): {email: string} => ({email});
+}
