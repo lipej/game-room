bc5e2e878330c97e18b22a6e7eb3eaa70ad477d0
Felipe José <lipej@pm.me>
2022-12-11 15:19:04 -0300

    feat: grpc add provider method

--- src/application/configs/grpc/shared/controller-adapter.ts
+++ src/application/configs/grpc/shared/controller-adapter.ts
@@ -1,15 +1,30 @@
+import {ErrorBase} from '#core/errors/base';
 import {BaseController, Response} from '#presentation/controllers/base';
+import {BaseMiddleware} from '#presentation/middlewares/base';
 import {sendUnaryData, ServerUnaryCall} from '@grpc/grpc-js';
 
 export class GRpcControllerAdapter {
   static adapt =
-    <T, K>(controller: BaseController<T, K>) =>
+    <T, K>(
+      controller: BaseController<T, K>,
+      ...middlewares: BaseMiddleware[]
+    ) =>
     async (
       call: ServerUnaryCall<T, Response<K>>,
       callback: sendUnaryData<Response<K>>
     ) => {
-      const result = await controller.execute(call.request);
+      try {
+        if (middlewares) {
+          for (const middleware of middlewares) {
+            await middleware.execute(call.request);
+          }
+        }
 
-      callback(null, result);
+        const result = await controller.execute(call.request);
+
+        callback(null, result);
+      } catch (err) {
+        callback(null, {success: false, error: <ErrorBase>err});
+      }
     };
 }
